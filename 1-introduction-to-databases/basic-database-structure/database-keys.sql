76d6ed7f832baf193412fb4cda525d0626d2d416
Marvin M. Pacis <marvedcodes@gmail.com>
2023-03-13 22:53:02 +0800

    Add code for database keys

--- 1-introduction-to-databases/basic-database-structure/database-keys.sql
+++ 1-introduction-to-databases/basic-database-structure/database-keys.sql
@@ -1,3 +1,13 @@
 ---------------------------------------------------------------------------------------------------------------------------------------------------
 -- TYPES OF KEYS IN A DATABASE
 ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- identify the main keys in tables in a rdb
+-- relationship between keys in a table
+
+-- 1. KEY ATTRIBUTE -> used to uniquely identify an individual record of data
+    -- a. Candidate key attribute -> any attribute that contains a unique value in each row of table
+    -- b. Composite key attribute -> a key composed of two or more atributes to form a unique value in each new row
+    -- c. Primary key -> a selected candidate key
+    -- d. Alternate key -> a candidate key not selected as the primary key
+    -- e. Foreign Key -> an attribute that references a unique key in another table
