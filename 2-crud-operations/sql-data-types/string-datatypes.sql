427a2033a2a4c3eec21d702fa436017a547bea0a
Marved <108392678+MarvedCodes@users.noreply.github.com>
2023-03-22 20:18:14 +0800

    Add another string data types

--- 2-crud-operations/sql-data-types/string-datatypes.sql
+++ 2-crud-operations/sql-data-types/string-datatypes.sql
@@ -11,3 +11,16 @@
 
 -- 2. VARCHAR
 -- -> chars of variable length
+
+
+-- 3. TINYTEXT 
+--  -> max of 255 chars
+
+-- 4. TEXT
+--  -> max of 65k chars
+
+-- 5. MEDIUMTEXT
+-- -> max of 16 million chars
+
+-- 6. LONGTEXT
+-- -> max of 4 billion chars
