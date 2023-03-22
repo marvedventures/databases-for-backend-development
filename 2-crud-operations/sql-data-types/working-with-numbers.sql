686a943fa27c35d0aa18951d3998730fb4b60283
Marved <108392678+MarvedCodes@users.noreply.github.com>
2023-03-22 19:47:04 +0800

    Refactor code

--- 2-crud-operations/sql-data-types/working-with-numbers.sql
+++ 2-crud-operations/sql-data-types/working-with-numbers.sql
@@ -35,7 +35,9 @@ USE cm_devices;
 -- 2. Identify the data type for each column within the table. Based on CM Mobiles’ requirements, the mobile devices table must contain three columns as follows:
     -- A column called “Device ID” which must store whole numbers. In this case you should use INT as the data type.
     -- A second column called “Device name” which is expected to store data as a string-like alphanumeric value. For example, iPhone XR 1.
-    -- And a third column called “Price”. This final column is expected to store numeric data with possible fractional values. In this case you should use DECIMAL data type. With the decimal data type, there is no issue with storing a whole number because the fractional part is separated by a decimal point with 00 on the right side of the number. This is indicated in the third row of the mobile devices table, where the price is 1050.00 (which is equivalent to 1050).
+    -- And a third column called “Price”. This final column is expected to store numeric data with possible fractional values. 
+    -- In this case you should use DECIMAL data type. With the decimal data type, there is no issue with storing a whole number because the fractional part is separated by a decimal point with 00 on the right side of the number. 
+    -- This is indicated in the third row of the mobile devices table, where the price is 1050.00 (which is equivalent to 1050).
 
 CREATE TABLE devices(deviceID int, deviceName varchar(50), price decimal);
 --------------------------------------------------------------------------------------------------------------------------------------------------
